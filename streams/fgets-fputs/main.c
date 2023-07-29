b5d7948af1409fe04717556e39701f86c61bdb0f
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-07-28 21:30:14 -0700

    added fgets/fputs example

---
+++ streams/fgets-fputs/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+const size_t BUFFER_SIZE = 256;
+
+
+int main(void)
+{
+    char buffer[BUFFER_SIZE];
+
+    fputs("Please input some lines, end with Ctrl-D:\n", stdout);
+
+    while(fgets(buffer, BUFFER_SIZE, stdin) != NULL)
+    {
+        fputs("You typed: ", stdout);
+        fputs(buffer, stdout);
+    }
+
+    return EXIT_SUCCESS;
+}

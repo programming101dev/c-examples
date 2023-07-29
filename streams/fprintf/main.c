b610522e74bb159952a3f00e006516879df651b5
programming101dev <113087924+programming101dev@users.noreply.github.com>
2023-07-29 10:22:49 -0700

    fprintf example

---
+++ streams/fprintf/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    int    age;
+    double height;
+
+    age    = 30;
+    height = 1.75;
+    fprintf(stdout, "My age is %d and my height is %.2f meters.\n", age, height);
+    fprintf(stderr, "Error: Invalid input. Expected an integer, but got '%s'.\n", "xyz");
+
+    return EXIT_SUCCESS;
+}

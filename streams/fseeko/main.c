badc5d629f005323e56c74893c67a14c6746d16c
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-07-30 21:09:08 -0700

    added more stream examples

---
+++ streams/fseeko/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void display_file(FILE* file, const char* message);
+
+
+int main(void)
+{
+    FILE *file;
+
+    file = fopen("../example.txt", "r");
+
+    if (file == NULL)
+    {
+        perror("Error opening the file");
+        return EXIT_FAILURE;
+    }
+
+    display_file(file, "File contents:\n\n");
+    fseeko(file, 0L, SEEK_SET);
+    display_file(file, "\n\nFile contents after SEEK_SET:\n\n");
+    fseeko(file, -10L, SEEK_CUR);
+    display_file(file, "\n\nFile contents after SEEK_CUR -10:\n\n");
+    fclose(file);
+    return EXIT_SUCCESS;
+}
+
+
+void display_file(FILE* file, const char* message)
+{
+    char ch;
+
+    fputs(message, stdout);
+
+    while ((ch = fgetc(file)) != EOF)
+    {
+        putchar(ch);
+    }
+}
---
+++ streams/rewrind/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void display_file(FILE* file, const char *message);
+
+
+int main(void)
+{
+    FILE *file;
+
+    file = fopen("../example.txt", "r");
+
+    if (file == NULL)
+    {
+        perror("Error opening the file");
+        return EXIT_FAILURE;
+    }
+
+    display_file(file, "File contents:\n\n");
+    rewind(file);
+    display_file(file, "\n\nFile contents after rewind:\n\n");
+    fclose(file);
+
+    return EXIT_SUCCESS;
+}
+
+
+void display_file(FILE* file, const char *message)
+{
+    char ch;
+
+    fputs(message, stdout);
+
+    while((ch = fgetc(file)) != EOF)
+    {
+        putchar(ch);
+    }
+}
+

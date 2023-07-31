f6ba03fdbe556b1ede002867b17606edbcb80bd9
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-07-30 21:16:41 -0700

    added ferror example

---
+++ streams/ferror/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *file;
+    const char buffer[] = "This is an attempt to write.";
+
+    file = fopen("../example.txt", "r");
+
+    if(file == NULL)
+    {
+        perror("Error opening the file");
+        return EXIT_FAILURE;
+    }
+
+    // Try to write to the file (this will fail in read mode)
+    if(fputs(buffer, file) == EOF)
+    {
+        if(ferror(file))
+        {
+            perror("Error writing to the file");
+        }
+        else
+        {
+            printf("End of file or other write error\n");
+        }
+    }
+
+    // Close the file
+    fclose(file);
+
+    return EXIT_SUCCESS;
+}

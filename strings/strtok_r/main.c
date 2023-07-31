d5f8b5b125af418a3f483e8d084ae0b9917a0a4b
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-07-31 12:51:08 -0700

    added strtok_r example

---
+++ strings/strtok_r/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+    char str[] = "This,is,a,test,string";
+    char *delimiter = ",";
+    char *token;
+    char *saveptr; // Used to save the context between calls to strtok_r
+
+    // First call to strtok_r with the input string and delimiter
+    token = strtok_r(str, delimiter, &saveptr);
+
+    while(token != NULL)
+    {
+        printf("Token: %s\n", token);
+
+        // Subsequent calls to strtok_r with NULL as the first argument
+        // to continue tokenizing the same string
+        token = strtok_r(NULL, delimiter, &saveptr);
+    }
+
+    // NOTE that strtok_r is destructive to the original string
+    printf("\nOriginal string \"%s\"\n", str);
+
+    return EXIT_SUCCESS;
+}

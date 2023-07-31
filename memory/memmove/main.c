7d26bb9f48e21a66039b763c2c06d22e6f3383c4
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-07-30 21:54:21 -0700

    added more memory examples

---
+++ memory/memmove/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    // Source data
+    int source_data[] = {1, 2, 3, 4, 5};
+    int num_elements;
+    int destination_buffer[5];
+
+    num_elements = sizeof(source_data) / sizeof(source_data[0]);
+    memmove(destination_buffer, source_data, num_elements * sizeof(source_data[0]));
+    printf("Copied elements: ");
+
+    for (int i = 0; i < num_elements; i++)
+    {
+        printf("%d ", destination_buffer[i]);
+    }
+
+    printf("\n");
+
+    return EXIT_SUCCESS;
+}

69a75bee4457aa6f3c499bdc86602d447c26f685
programming101dev <113087924+programming101dev@users.noreply.github.com>
2023-08-06 19:23:54 -0700

    delete any .out file

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Find all directories that contain a.out files, sort them, and iterate through them
-find . -name ".out" -exec dirname {} \; | sort -u | while read -r dir; do
-  echo "Deleting a.out in directory: $dir"
-  find "$dir" -maxdepth 1 -name ".out" -type f | sort | while read -r file; do
-    rm "$file"
-    if [ $? -ne 0 ]; then
-      echo "Deletion failed for $file. Exiting."
-      exit 1
-    fi
-  done
+# Find and delete all files with the .out extension
+find . -name "*.out" -type f | while read -r file; do
+  echo "Deleting $file"
+  rm "$file"
+  if [ $? -ne 0 ]; then
+    echo "Deletion failed for $file. Exiting."
+    exit 1
+  fi
 done
 
 echo "Deletion complete."

2715d1814acbc3126cc458a52dcb5804ac5083f4
programming101dev <113087924+programming101dev@users.noreply.github.com>
2023-08-20 10:46:35 -0700

    added portable data type examples

---
+++ portable-types/int16_t/main.c
@@ -0,0 +1,34 @@
+/*
+ * This code is licensed under the Attribution-NonCommercial-NoDerivatives 4.0 International license.
+ *
+ * Author: D'Arcy Smith (ds@programming101.dev)
+ *
+ * You are free to:
+ *   - Share: Copy and redistribute the material in any medium or format.
+ *   - Under the following terms:
+ *       - Attribution: You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+ *       - NonCommercial: You may not use the material for commercial purposes.
+ *       - NoDerivatives: If you remix, transform, or build upon the material, you may not distribute the modified material.
+ *
+ * For more details, please refer to the full license text at:
+ * https://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    int16_t value = 42;
+
+    printf("Using PRId16 (decimal): %" PRId16 "\n", value);
+    printf("Using PRIi16 (decimal): %" PRIi16 "\n", value);
+    printf("Using PRIu16 (unsigned): %" PRIu16 "\n", (uint16_t)value);
+    printf("Using PRIx16 (hexadecimal): %" PRIx16 "\n", (uint16_t)value);
+    printf("Using PRIX16 (uppercase hexadecimal): %" PRIX16 "\n", (uint16_t)value);
+
+    return EXIT_SUCCESS;
+}
---
+++ portable-types/int32_t/main.c
@@ -0,0 +1,34 @@
+/*
+ * This code is licensed under the Attribution-NonCommercial-NoDerivatives 4.0 International license.
+ *
+ * Author: D'Arcy Smith (ds@programming101.dev)
+ *
+ * You are free to:
+ *   - Share: Copy and redistribute the material in any medium or format.
+ *   - Under the following terms:
+ *       - Attribution: You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+ *       - NonCommercial: You may not use the material for commercial purposes.
+ *       - NoDerivatives: If you remix, transform, or build upon the material, you may not distribute the modified material.
+ *
+ * For more details, please refer to the full license text at:
+ * https://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    int32_t value = 42;
+
+    printf("Using PRId32 (decimal): %" PRId32 "\n", value);
+    printf("Using PRIi32 (decimal): %" PRIi32 "\n", value);
+    printf("Using PRIu32 (unsigned): %" PRIu32 "\n", (uint32_t)value);
+    printf("Using PRIx32 (hexadecimal): %" PRIx32 "\n", (uint32_t)value);
+    printf("Using PRIX32 (uppercase hexadecimal): %" PRIX32 "\n", (uint32_t)value);
+
+    return EXIT_SUCCESS;
+}

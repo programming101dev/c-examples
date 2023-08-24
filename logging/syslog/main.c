e172f734b75777c5a7b23cba3c2e881d7763ce13
programming101dev <113087924+programming101dev@users.noreply.github.com>
2023-08-24 14:56:39 -0700

    added logging exmaples

---
+++ logging/syslog/main.c
@@ -0,0 +1,39 @@
+/*
+ * This code is licensed under the Attribution-NonCommercial-NoDerivatives 4.0 International license.
+ *
+ * Author: D'Arcy Smith (ds@programming101.dev)
+ *
+ * You are free to:
+ *   - Share: Copy and redistribute the material in any medium or format.
+ *   - Under the following terms:
+ *       - Attribution: You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+ *       - NonCommercial: You may not use the material for commercial purposes.
+ *       - NoDerivatives: If you remix, transform, or build upon the material, you may not distribute the modified material.
+ *
+ * For more details, please refer to the full license text at:
+ * https://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+    openlog("log-demo", LOG_PID, LOG_USER);
+    printf("Logging PID %d\n", getpid());
+    syslog(LOG_EMERG, "This is an emergency message.");
+    syslog(LOG_ALERT, "This is an alert message.");
+    syslog(LOG_CRIT, "This is a critical message.");
+    syslog(LOG_ERR, "This is an error message.");
+    syslog(LOG_WARNING, "This is a warning message.");
+    syslog(LOG_NOTICE, "This is a notice message.");
+    syslog(LOG_INFO, "This is an informational message.");
+    syslog(LOG_DEBUG, "This is a debug message.");
+    closelog();
+    
+    return EXIT_SUCCESS;
+}

33272a06ad334c2b7d8e5bf1b416a9e3e950d712
tofubeer <ds@terratap.com>
2023-09-30 09:35:02 -0700

    delete everything on macOS

--- generate-makefiles.sh
+++ generate-makefiles.sh
@@ -3,9 +3,15 @@
 # Function to determine the platform and set the shared library extension accordingly
 get_shared_lib_extension() {
     case "$(uname -s)" in
-        Linux) SHARED_EXT=".so" ;;
-        Darwin) SHARED_EXT=".dylib" ;;
-        FreeBSD) SHARED_EXT=".so" ;;
+        Linux)
+            SHARED_EXT=".so"
+            BINARY_EXT="" ;;
+        Darwin)
+            SHARED_EXT=".dylib"
+            BINARY_EXT=".dSYM" ;;
+        FreeBSD)
+            SHARED_EXT=".so"
+            BINARY_EXT="" ;;
         *)
             echo "Unsupported platform"
             exit 1
@@ -14,7 +20,8 @@ get_shared_lib_extension() {
 }
 
 # Function to check if a flag is supported, considering flag dependencies
-is_flag_supported() {
+is_flag_supported()
+{
     local flag="$1"
     local dependencies="$2"  # Additional dependencies to consider
 
@@ -1003,37 +1010,43 @@ populate_supported_flags() {
 }
 
 # Function to generate the Makefile in the current directory
-generate_makefile() {
-  # Determine the library names based on the platform
-      # Define LIBRARIES based on the platform
-      case "$(uname -s)" in
-          Linux) LIBRARIES="" ;;
-          Darwin) LIBRARIES="" ;;
-          FreeBSD)
-              # Check if the directory name starts with "pthread"
-              if [[ "$(basename "$(pwd)")" == pthread* ]]; then
-                  LIBRARIES="-lpthread"
-              else
-                  LIBRARIES=""
-              fi
-              ;;
-          *)
-              echo "Unsupported platform"
-              exit 1
-              ;;
-      esac
+generate_makefile()
+{
+    # Determine the library names based on the platform
+    # Define LIBRARIES based on the platform
+    case "$(uname -s)" in
+        Linux) LIBRARIES="" ;;
+        Darwin) LIBRARIES="" ;;
+        FreeBSD)
+            # Check if the directory name starts with "pthread"
+            if [[ "$(basename "$(pwd)")" == pthread* ]]; then
+                LIBRARIES="-lpthread"
+            else
+                LIBRARIES=""
+            fi
+            ;;
+        *)
+            echo "Unsupported platform"
+            exit 1
+            ;;
+    esac
 
     # Initialize the Makefile
     echo -e ".PHONY: main main-traceable clean lint all\n" > Makefile
-    echo "CC=$CC" >> Makefile
-    echo "COMPILATION_FLAGS=-std=c18 -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_DEFAULT_SOURCE -D_DARWIN_C_SOURCE -D_GNU_SOURCE -D__BSD_VISIBLE -Werror" >> Makefile
-    echo "SUPPORTED_WARNING_FLAGS=${SUPPORTED_WARNING_FLAGS[@]}" >> Makefile
-    echo "SUPPORTED_SANITIZER_FLAGS=${SUPPORTED_SANITIZER_FLAGS[@]}" >> Makefile
-    echo "SUPPORTED_ANALYZER_FLAGS=${SUPPORTED_ANALYZER_FLAGS[@]}" >> Makefile
-    echo "SUPPORTED_DEBUG_FLAGS=${SUPPORTED_DEBUG_FLAGS[@]}" >> Makefile
-    echo "CLANG_TIDY_CHECKS=-checks=*,-llvmlibc-restrict-system-libc-headers,-altera-struct-pack-align,-readability-identifier-length,-altera-unroll-loops,-cppcoreguidelines-init-variables,-cert-err33-c,-modernize-macro-to-enum,-bugprone-easily-swappable-parameters,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,-altera-id-dependent-backward-branch,-concurrency-mt-unsafe,-misc-unused-parameters,-hicpp-signed-bitwise,-google-readability-todo,-cert-msc30-c,-cert-msc50-cpp,-readability-function-cognitive-complexity,-clang-analyzer-security.insecureAPI.strcpy,-cert-env33-c,-android-cloexec-accept,-clang-analyzer-security.insecureAPI.rand,-misc-include-cleaner" >> Makefile
-    echo "LIBRARIES=${LIBRARIES}" >> Makefile
-    echo "PROGRAMS=" >> Makefile
+    echo -e "CC=$CC" >> Makefile
+    echo -e "COMPILATION_FLAGS=-std=c18 -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_DEFAULT_SOURCE -D_DARWIN_C_SOURCE -D_GNU_SOURCE -D__BSD_VISIBLE -Werror" >> Makefile
+    echo -e "SUPPORTED_WARNING_FLAGS=${SUPPORTED_WARNING_FLAGS[@]}" >> Makefile
+    echo -e "SUPPORTED_SANITIZER_FLAGS=${SUPPORTED_SANITIZER_FLAGS[@]}" >> Makefile
+    echo -e "SUPPORTED_ANALYZER_FLAGS=${SUPPORTED_ANALYZER_FLAGS[@]}" >> Makefile
+    echo -e "SUPPORTED_DEBUG_FLAGS=${SUPPORTED_DEBUG_FLAGS[@]}" >> Makefile
+    echo -e "CLANG_TIDY_CHECKS=-checks=*,-llvmlibc-restrict-system-libc-headers,-altera-struct-pack-align,-readability-identifier-length,-altera-unroll-loops,-cppcoreguidelines-init-variables,-cert-err33-c,-modernize-macro-to-enum,-bugprone-easily-swappable-parameters,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,-altera-id-dependent-backward-branch,-concurrency-mt-unsafe,-misc-unused-parameters,-hicpp-signed-bitwise,-google-readability-todo,-cert-msc30-c,-cert-msc50-cpp,-readability-function-cognitive-complexity,-clang-analyzer-security.insecureAPI.strcpy,-cert-env33-c,-android-cloexec-accept,-clang-analyzer-security.insecureAPI.rand,-misc-include-cleaner" >> Makefile
+    echo -e "LIBRARIES=${LIBRARIES}" >> Makefile
+    echo -e "PROGRAMS=" >> Makefile
+
+    if [ -n "$BINARY_EXT" ]; then
+        echo -e "BINARIES=" >> Makefile
+    fi
+
     echo "LIBS=" >> Makefile
     echo -e "SOURCES=\n" >> Makefile
 
@@ -1051,17 +1064,17 @@ generate_makefile() {
                 # Generate a shared library rule with the appropriate extension
                 echo -e "lib$filename$SHARED_EXT: $file" >> Makefile
                 echo -e "\t@\$(CC) \$(COMPILATION_FLAGS) \$(CFLAGS) \$(SUPPORTED_WARNING_FLAGS) \$(SUPPORTED_SANITIZER_FLAGS) \$(SUPPORTED_ANALYZER_FLAGS) \$(SUPPORTED_DEBUG_FLAGS) -shared -fPIC -o lib$filename$SHARED_EXT $file \$(LIBRARIES)" >> Makefile
-                echo "LIBS += lib$filename$SHARED_EXT" >> Makefile
+                echo -e "LIBS += lib$filename$SHARED_EXT" >> Makefile
 
                 # Generate a shared library rule with the appropriate extension
                 echo -e "lib$filename-traceaable$SHARED_EXT: $file" >> Makefile
                 echo -e "\t@\$(CC) \$(COMPILATION_FLAGS) \$(CFLAGS) \$(SUPPORTED_WARNING_FLAGS) \$(SUPPORTED_SANITIZER_FLAGS) \$(SUPPORTED_ANALYZER_FLAGS) \$(SUPPORTED_DEBUG_FLAGS)  -shared -fPIC -o lib$filename-traceaable$SHARED_EXT $file \$(LIBRARIES)" >> Makefile
-                echo "LIBS += lib$filename-traceaable$SHARED_EXT" >> Makefile
+                echo -e "LIBS += lib$filename-traceaable$SHARED_EXT\n" >> Makefile
             else
                 if [[ "$CC" == "gcc"* ]]; then
                     if [[ "$second_to_last_dir/$last_dir/$file" == "memory/malloc-free/main.c" || "$second_to_last_dir/$last_dir/$file" == "memory/memset/main.c" ]]; then
                         # Add the additional flag for files in the specified directories
-                        echo -e COMPILATION_FLAGS+="-Wno-analyzer-use-of-uninitialized-value -Wno-sometimes-uninitialized" >> Makefile
+                        echo COMPILATION_FLAGS+="-Wno-analyzer-use-of-uninitialized-value -Wno-sometimes-uninitialized" >> Makefile
                     fi
                 fi
 
@@ -1070,23 +1083,32 @@ generate_makefile() {
                 # Generate an executable rule with the supported warning flags
                 echo "$filename: $file" >> Makefile
                 echo -e "\t@\$(CC) \$(COMPILATION_FLAGS) \$(CFLAGS) \$(SUPPORTED_WARNING_FLAGS) \$(SUPPORTED_SANITIZER_FLAGS) \$(SUPPORTED_ANALYZER_FLAGS) \$(SUPPORTED_DEBUG_FLAGS)  -o $filename $file \$(LIBRARIES)" >> Makefile
-                echo -e "PROGRAMS += $filename\n" >> Makefile
+                echo -e "PROGRAMS += $filename" >> Makefile
+                echo -e "BINARIES += $filename$BINARY_EXT\n" >> Makefile
 
                 # Generate a traceable version rule
-                echo "$filename-traceable: $file" >> Makefile
+                echo -e "$filename-traceable: $file" >> Makefile
                 echo -e "\t@\$(CC) \$(COMPILATION_FLAGS) \$(CFLAGS) \$(SUPPORTED_WARNING_FLAGS) \$(SUPPORTED_ANALYZER_FLAGS) \$(SUPPORTED_DEBUG_FLAGS)  -o $filename-traceable $file \$(LIBRARIES)" >> Makefile
-                echo -e "PROGRAMS += $filename-traceable\n" >> Makefile
+                echo -e "PROGRAMS += $filename-traceable" >> Makefile
+
+                if [ -n "$BINARY_EXT" ]; then
+                    echo -e "BINARIES += $filename-traceable$BINARY_EXT\n" >> Makefile
+                fi
             fi
         fi
     done
+
+    # Add a lint rule to remove all generated binaries and libraries
+    echo -e "\nlint:" >> Makefile
+	  echo -e "\t@\$(CLANGTIDY) \$(SOURCES) -quiet --warnings-as-errors='*' \$(CLANG_TIDY_CHECKS) -- \$(COMPILATION_FLAGS) \$(CFLAGS) \$(SUPPORTED_WARNING_FLAGS) \$(SUPPORTED_DEBUG_FLAGS) \$(LIBRARIES)" >> Makefile
 
     # Add a clean rule to remove all generated binaries and libraries
     echo -e "clean:" >> Makefile
     echo -e "\t@rm -f \$(PROGRAMS) \$(LIBS)" >> Makefile
 
-    # Add a lint rule to remove all generated binaries and libraries
-    echo -e "\nlint:" >> Makefile
-	  echo -e "\t@\$(CLANGTIDY) \$(SOURCES) -quiet --warnings-as-errors='*' \$(CLANG_TIDY_CHECKS) -- \$(COMPILATION_FLAGS) \$(CFLAGS) \$(SUPPORTED_WARNING_FLAGS) \$(SUPPORTED_DEBUG_FLAGS) \$(LIBRARIES)" >> Makefile
+    if [ -n "$BINARY_EXT" ]; then
+        echo -e "\t@rm -rf \$(BINARIES)" >> Makefile
+    fi
 
     # Add an "all" rule to build all programs and libraries
     echo -e "\nall: \$(PROGRAMS) \$(LIBS) lint" >> Makefile
@@ -1096,7 +1118,8 @@ generate_makefile() {
 }
 
 # Function to process directories recursively
-process_directories() {
+process_directories()
+{
     local dir="$1"
 
     # Loop through each subdirectory

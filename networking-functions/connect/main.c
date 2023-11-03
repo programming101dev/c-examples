54e1cb4d7130f69ab747fa14c55575a73dd26ede
tofubeer <ds@terratap.com>
2023-11-02 22:11:11 -0700

    fixed address length to connect

--- networking-functions/connect/main.c
+++ networking-functions/connect/main.c
@@ -208,6 +208,7 @@ static void socket_connect(int sockfd, struct sockaddr_storage *addr, in_port_t 
 {
     char      addr_str[INET6_ADDRSTRLEN];
     in_port_t net_port;
+    socklen_t addr_len;
 
     if(inet_ntop(addr->ss_family, addr->ss_family == AF_INET ? (void *)&(((struct sockaddr_in *)addr)->sin_addr) : (void *)&(((struct sockaddr_in6 *)addr)->sin6_addr), addr_str, sizeof(addr_str)) == NULL)
     {
@@ -224,6 +225,7 @@ static void socket_connect(int sockfd, struct sockaddr_storage *addr, in_port_t 
 
         ipv4_addr           = (struct sockaddr_in *)addr;
         ipv4_addr->sin_port = net_port;
+        addr_len            = sizeof(struct sockaddr_in);
     }
     else if(addr->ss_family == AF_INET6)
     {
@@ -231,6 +233,7 @@ static void socket_connect(int sockfd, struct sockaddr_storage *addr, in_port_t 
 
         ipv6_addr            = (struct sockaddr_in6 *)addr;
         ipv6_addr->sin6_port = net_port;
+        addr_len             = sizeof(struct sockaddr_in6);
     }
     else
     {
@@ -238,9 +241,12 @@ static void socket_connect(int sockfd, struct sockaddr_storage *addr, in_port_t 
         exit(EXIT_FAILURE);
     }
 
-    if(connect(sockfd, (struct sockaddr *)addr, sizeof(*addr)) == -1)
+    if(connect(sockfd, (struct sockaddr *)addr, addr_len) == -1)
     {
-        perror("connect");
+        char *msg;
+
+        msg = strerror(errno);
+        fprintf(stderr, "Error: connect (%d): %s\n", errno, msg);
         exit(EXIT_FAILURE);
     }
 
